77498e5ef67229a0a59a7fc4ea431be93ec5dca0
Disha <dishanemade04@gmail.com>
2026-09-24 14:53:24 +0530

    update project

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
 from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, AIMessage
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
+"""chatbot with proper history"""
 llm=ChatGroq(model="openai/gpt-oss-20b")
 
 print("My first Chatbot")
 
+history=[]
+
 while True:
+
     prompt=input("user: ")
+    history.append(HumanMessage(content=prompt))
     if prompt.lower()=="exit":
         break
-    
     response=llm.invoke(prompt)
+    history.append(AIMessage(content=response.content))
     print(response.content)
+
+print(history)
